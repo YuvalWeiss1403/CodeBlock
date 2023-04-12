f050362064aa93e3d1b99b1896cbe0d37d0e0543
Yuval Weiss <108930250+YuvalWeiss1403@users.noreply.github.com>
2023-04-12 22:51:28 +0300

    first homepage

---
+++ src/Components/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import "../HomePage/HomePage.css";
+import Navbar from "../NavBar/NavBar";
+
+const HomePage: React.FC = () => {
+	return (
+		<div className="home-page">
+			<Navbar />
+		</div>
+	);
+};
+
+export default HomePage;
